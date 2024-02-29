f1e1d0a783e451be980985d88f207d195307af95
DaniilR <daniil.r4@yandex.ru>
2024-02-29 22:49:17 +0300

    Trying to add func tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,39 +1,20 @@
 import pytest
 
-from testsuite.databases import pgsql
-
-
 # Start the tests via `make test-debug` or `make test-release`
 
+async def test_start(service_client):
+    response = await service_client.post(
+        '/v1/start',
+    )
+    assert response.status == 200
+    assert len(response.text) == 14
 
 async def test_first_time_users(service_client):
+    token = await service_client.post('v1/start').text
+
     response = await service_client.post(
-        '/v1/hello',
-        params={'name': 'userver'},
+        '/v1/check',
+        params={'token': token, 'word': 'horse'},
     )
     assert response.status == 200
-    assert response.text == 'Hello, userver!\n'
 
-
-async def test_db_updates(service_client):
-    response = await service_client.post('/v1/hello', params={'name': 'World'})
-    assert response.status == 200
-    assert response.text == 'Hello, World!\n'
-
-    response = await service_client.post('/v1/hello', params={'name': 'World'})
-    assert response.status == 200
-    assert response.text == 'Hi again, World!\n'
-
-    response = await service_client.post('/v1/hello', params={'name': 'World'})
-    assert response.status == 200
-    assert response.text == 'Hi again, World!\n'
-
-
-@pytest.mark.pgsql('db_1', files=['initial_data.sql'])
-async def test_db_initial_data(service_client):
-    response = await service_client.post(
-        '/v1/hello',
-        params={'name': 'user-from-initial_data.sql'},
-    )
-    assert response.status == 200
-    assert response.text == 'Hi again, user-from-initial_data.sql!\n'
